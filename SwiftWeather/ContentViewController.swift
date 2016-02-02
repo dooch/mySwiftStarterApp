b6fa746b81f6dbcc8af207d47344cd0685e2450e
theDooch <cb@CBs-MacBook-Pro.local>
2016-02-02 13:30:34 +1100

    Added a Navigation Controller to the Project.
    Added a Navigation Controller to the Project. It is still a little
    messy, but taking baby steps for now.

--- SwiftWeather/ContentViewController.swift
+++ SwiftWeather/ContentViewController.swift
@@ -27,9 +27,6 @@ class ContentViewController: UIViewController {
         
     }
 
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ SwiftWeather/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  SwiftWeather
+//
+//  Created by CB on 2/02/2016.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view, typically from a nib
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
